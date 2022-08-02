72347e1a45c698c9298b66cc72ea1f256b897869
june0605 <june0100726@gmail.com>
2022-08-02 16:54:45 +0900

    Issue(#4) Add test.py

---
+++ test.py
@@ -0,0 +1,24 @@
+import numpy as np
+import tensorflow as tf
+import keras
+import glob
+
+model = keras.models.load_model('./dog_vs_not_dog')
+dog_images = glob.glob('.\\data\\dog_test_data\\*')
+score = [0,0]
+i = 0
+for image in dog_images:
+
+    img=tf.keras.preprocessing.image.load_img(image,target_size=(300, 300))
+    x=tf.keras.preprocessing.image.img_to_array(img)
+    x=np.expand_dims(x, axis=0)
+    images = np.vstack([x])
+    classes = model.predict(images)
+    if classes[0][0] == 1:
+        score[0] += 1    
+    else :
+        score[1] += 1
+    i += 1
+    if i==100:
+        break
+print(score)
