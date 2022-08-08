b987343ad94d2e7d84a6041f7cce003068ce8513
june0605 <june0100726@gmail.com>
2022-08-08 13:25:38 +0900

    Issue(#6) Add checking_test-data_fuction

--- model_h5_save.py
+++ model_h5_save.py
@@ -1,4 +1,11 @@
 import keras
 
-model = keras.models.load_model('./dog_vs_not_dog_v3')
-model.save('./h5_model/dog_vs_not_dog_v3.h5')
+import tensorflow_hub as hub
+
+#model = tf.saved_model.load('./dog_vs_not_dog_v4')
+#model = keras.models.load_model('./dog_vs_not_dog_v3')
+model = keras.Sequential([
+    hub.KerasLayer("./dog_vs_not_dog_v4")
+])
+
+model.save('./h5_model/dog_vs_not_dog_v4.h5')
--- test.py
+++ test.py
@@ -1,10 +1,22 @@
 import numpy as np
 import tensorflow as tf
-import keras
 import glob
+import tensorflow_hub as hub
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+import matplotlib.image as mpimg
 
-model = keras.models.load_model('./dog_vs_not_dog_v4')
+#model = tf.saved_model.load('./dog_vs_not_dog_v4')
+#model = keras.models.load_model('./dog_vs_not_dog_v3')
+model = tf.keras.Sequential([
+    hub.KerasLayer("./dog_vs_not_dog_v4")
+])
+
 dog_images = glob.glob('.\\data\\dog_test_data\\*')
+
+error_dataset = []
+
 score = [0,0]
 i = 0
 for image in dog_images:
@@ -13,13 +25,35 @@ for image in dog_images:
     x=tf.keras.preprocessing.image.img_to_array(img)
     x=np.expand_dims(x, axis=0)
     images = np.vstack([x])
-    classes = model.predict(images)
+    classes = model.predict(images, verbose=1)
     if classes[0] == 1 :
         score[0] += 1    
+        None
     else :
         score[1] += 1
-    i += 1
-    '''if i==100:
-       break'''
+        cv2_image = cv2.imread(image, cv2.COLOR_BGR2RGB)    
+        cv2_image2 = cv2.cvtColor(cv2_image, cv2.COLOR_BGR2RGB) 
+        error_dataset.append(cv2_image2)
 
+    i += 1
+    '''
+    if i ==10000:
+        break'''
 print(score)
+
+
+
+nrows = 10
+ncols = 10
+
+fig = plt.gcf()
+fig.set_size_inches(nrows * 4, ncols * 4)
+j = 0
+for j in range(len(error_dataset)):
+
+  sp = plt.subplot(nrows,ncols, j + 1)
+  sp.axis('Off')
+  plt.imshow(error_dataset[j])
+
+plt.show()
+
