1cc33cf2c7c4e7c4f4136b08c23a60a392230f86
june0605 <june0100726@gmail.com>
2022-11-03 13:32:54 +0900

    Add dog_vs_not_dog model_v5

--- model_h5_save.py
+++ model_h5_save.py
@@ -4,8 +4,11 @@ import tensorflow_hub as hub
 
 #model = tf.saved_model.load('./dog_vs_not_dog_v4')
 #model = keras.models.load_model('./dog_vs_not_dog_v3')
+'''
 model = keras.Sequential([
     hub.KerasLayer("./dog_vs_not_dog_v4")
 ])
+'''
+model = keras.models.load_model('./dog_vs_not_dog_v5')
 
-model.save('./h5_model/dog_vs_not_dog_v4.h5')
+model.save('./h5_model/dog_vs_not_dog_v5.h5')
--- test2.py
+++ test2.py
@@ -13,7 +13,6 @@ test_image = tf.keras.utils.img_to_array(test_image)
 test_image = test_image.reshape((1, test_image.shape[0], test_image.shape[1], test_image.shape[2]))
 test_image = preprocess_input(test_image)
 prediction = model.predict(test_image)
-
 df = pd.DataFrame({'pred':prediction[0]})
 df = df.sort_values(by='pred', ascending=False, na_position='first')
 print(f"## 예측률 : {(df.iloc[0]['pred'])* 100:.2f}%")
